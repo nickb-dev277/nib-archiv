82fcb73dc85e37550dba6589e6124ae889c7ee23
nickb-dev277 <nickbruns18@gmail.com>
2026-08-28 01:48:52 +0200

    Change header position from right to left

--- html.js
+++ html.js
@@ -248,11 +248,10 @@ label select {
 /* ─────────────────────────────────────
    Sprachschalter
    ───────────────────────────────────── */
-
 .public-header-tools {
   position: absolute;
   top: 0;
-  right: 0;
+  left: 0;
 
   display: flex;
   align-items: center;
